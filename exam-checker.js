5bcc5d9d04757ab0bce5c0f872b13e6d5255a915
Mary Zhavnerovich <rostova0106@gmail.com>
2019-09-05 22:02:07 +0300

    why don't like my job

---
+++ exam-checker.js
@@ -0,0 +1,13 @@
+function checkExam(array1, array2) {
+ let array1 = [ ["a", "a", "b", "d"]
+ let array2 =  ["b", "a", "a", "d"];
+ let score=0;
+ let ln1 = array1.length;
+ 
+ for (let j=0; j<ln1, j++){
+ if array1[j]===array2[j]{
+ return score++4;
+ } else array1[j]!===array2[j]{
+ return score--1;
+ };
+}
